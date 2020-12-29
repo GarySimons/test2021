5bad27759ca0614819421759f7b1a37fe61515ef
GarySimons <garycsimons@gmail.com>
2020-12-29 12:40:48 +0000

    Using template inheritance to drop nav content onto each page

--- run.py
+++ run.py
@@ -3,13 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# app route for home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# app route for about page
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+# app route for contact page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
